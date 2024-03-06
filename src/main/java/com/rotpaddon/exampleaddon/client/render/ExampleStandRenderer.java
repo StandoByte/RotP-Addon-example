cff149244d0132742ee1d2ab03ce6db4eba0a9b8
StandoByte <94539965+StandoByte@users.noreply.github.com>
2024-03-07 02:09:29 +0300

    Edited Stand entity renderer generic parameter

--- src/main/java/com/rotpaddon/exampleaddon/client/render/ExampleStandRenderer.java
+++ src/main/java/com/rotpaddon/exampleaddon/client/render/ExampleStandRenderer.java
@@ -1,5 +1,6 @@
 package com.rotpaddon.exampleaddon.client.render;
 
+import com.github.standobyte.jojo.client.render.entity.model.stand.StandEntityModel;
 import com.github.standobyte.jojo.client.render.entity.renderer.stand.StandEntityRenderer;
 import com.rotpaddon.exampleaddon.AddonMain;
 import com.rotpaddon.exampleaddon.entity.ExampleStandEntity;
@@ -7,7 +8,7 @@ import com.rotpaddon.exampleaddon.entity.ExampleStandEntity;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.util.ResourceLocation;
 
-public class ExampleStandRenderer extends StandEntityRenderer<ExampleStandEntity, ExampleStandModel> {
+public class ExampleStandRenderer extends StandEntityRenderer<ExampleStandEntity, StandEntityModel<ExampleStandEntity>> {
     
     public ExampleStandRenderer(EntityRendererManager renderManager) {
         super(renderManager, new ExampleStandModel(), 
